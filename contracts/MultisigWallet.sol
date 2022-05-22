0f9fb5cc6cf323f5df50c04ecc848b96e8323164
"Leonardo" <"leonardo.fig.almeida@gmail.com">
2022-05-21 21:29:03 -0300

    Some documentation...

---
+++ contracts/MultisigWallet.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+
+/**
+ * This Smart Contract implements a Multisig Wallet.
+ * Any user can deposit money from his account.
+ * But withdrawls require n-of-N approvals to go through.
+ */
+contract MultisigWallet {
+    
+}
